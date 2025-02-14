6f98d34991004f988001897ae56c7b6a7497d9f1
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-02-13 21:48:13 -0700

    fix(elements): fetch the clock elements through their titles instead of their classNames

--- content.js
+++ content.js
@@ -15,19 +15,24 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
   
 function getTimeWorkedSoFar() {
-    let elements = document.getElementsByClassName("WMTM")
-    if (elements[0] != null && elements[0].innerText != '') {
-        return elements[0].innerText
-    } else {
-        elements = document.getElementsByClassName("WNTM")
-        return elements[0] ? elements[0].innerText : null
+    let elements = document.getElementsByTagName('span')
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].title.startsWith('This Week')) {
+            return elements[i].title
+        }
     }
-    
 }
 
 function getTimeClockedIn() {
-    const elements = document.querySelectorAll('.gwt-Label', '.WEOO', '.WNMO')
-    return elements[0] ? elements[0].innerText : null
+    let elements = document.getElementsByTagName('div')
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].title.startsWith('Checked In at')) {
+            return [elements[i].title, 'checked-in']
+        }
+        if (elements[i].title.startsWith('Checked Out at')) {
+            return [elements[i].title, 'checked-out']
+        }
+    }
 }
 
 // Gather the target number from a string
@@ -41,13 +46,22 @@ function getCurrentHours() {
 }
 
 function getClockIn() {
-    const text = getTimeClockedIn()
-    const numString = text.substring(14, text.length - 3)
-    const numsSplit = numString.split(':')
-    let timestamp = Number(numsSplit[0])
-    timestamp += (Number(numsSplit[1]) / 60)
-    timestamp += (Number(numsSplit[2]) / 3600)
-    return timestamp
+    const [text, type] = getTimeClockedIn()
+    let numString
+    if (type === 'checked-in') {
+        numString = text.substring(14, text.length - 3)
+        const numsSplit = numString.split(':')
+        let timestamp = Number(numsSplit[0])
+        timestamp += (Number(numsSplit[1]) / 60)
+        timestamp += (Number(numsSplit[2]) / 3600)
+        return timestamp
+    } else {
+        const d = new Date(Date.now())
+        let timestamp = d.getHours() % 12
+        timestamp += d.getMinutes() / 60
+        timestamp += d.getSeconds() / 3600
+        return timestamp
+    }    
 }
 
 function sendNumToString(inputNum) {
@@ -58,7 +72,7 @@ function calculateClockOutTime(clockIn, timeRemaining) {
     const clockOutTime = clockIn + timeRemaining
 
     const secondsRemaining = Math.round(clockOutTime * 3600)
-    const hours = Math.floor(clockOutTime)
+    const hours = Math.floor(clockOutTime) % 12
     const minutes = Math.floor((secondsRemaining % 3600) / 60)
     const seconds = secondsRemaining % 60
 
